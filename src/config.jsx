0e46c1ff0aa45312c3a0422f0a70b13ceb2b334c
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-02-01 22:05:22 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,5 @@
 // Define the base API URL here
-const API_BASE_URL = 'http://localhost:8000'; // Replace with your actual API URL
+const API_BASE_URL = 'http://intern.monster:8000'; // Replace with your actual API URL
 const API_ROUTE_URL = 'http://localhost:5173'; 
 
 export {API_ROUTE_URL}
