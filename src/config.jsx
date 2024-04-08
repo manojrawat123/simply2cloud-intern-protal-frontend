15bb94f21b3e9c71863833004a098651c2907a59
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-04-08 10:53:44 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,7 @@
 // Define the base API URL here
 // const API_BASE_URL = 'https://kingbahimunna.pythonanywhere.com'; 
 
-const API_BASE_URL = 'http://kingbahimunna.pythonanywhere.com'; 
+const API_BASE_URL = 'https://kingbahimunna.pythonanywhere.com'; 
 const API_ROUTE_URL = 'https://simply2cloud-intern-protal-frontend.vercel.app'; 
 
 export {API_ROUTE_URL}
